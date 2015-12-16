5a0a2c0df4c0cbace20cb0afa80d090c184209be
Philippe Coval <philippe.coval@osg.samsung.com>
2015-12-16 14:23:44 +0000

    wip: mod: rules/config.mk (sandbox/pcoval/oic)
    Change-Id: I5801f3134943edc6bb6e1d9445085d68c2197965
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/config.mk
+++ rules/config.mk
@@ -6,10 +6,12 @@ MACHINE?=qemux86
 machine?=${MACHINE}
 
 branch?=tizen
-repo_branch?=${branch}
+repo_branch_name?=oic
+repo_branch?=sandbox/pcoval/${repo_branch_name}
 
 distro?=${branch}-distro
 init_name?=${branch}-common
-image?=${init_name}-core-image-minimal
+image?=${branch}-${repo_branch_name}
+
 init_build_env?=${sources_dir}/${distro}/${init_name}-init-build-env
 
