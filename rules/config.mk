84f5ea238f71aeef41719f5e7651737cb95c8447
Philippe Coval <philippe.coval@osg.samsung.com>
2015-12-17 02:14:03 +0000

    wip: mod: rules/config.mk (tizen-rasbperrypi)
    Change-Id: I5424abc58dc9e43774590e4fa2bf02b49d0ae8a8
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/config.mk
+++ rules/config.mk
@@ -1,15 +1,16 @@
 #! /usr/bin/make -f
 # Author: Philippe Coval <philippe.coval@osg.samsung.com>
 # ex: set tabstop=4 noexpandtab:
-
-MACHINE?=qemux86
+bsp?=raspberrypi
+MACHINE?=${bsp}2
 machine?=${MACHINE}
 
-branch?=tizen
+os=tizen
+os_profile=common
+branch?=${os}-${bsp}
 repo_branch?=${branch}
-
-distro?=${branch}-distro
-init_name?=${branch}-common
-image?=${init_name}-core-image-minimal
+distro?=${os}-distro
+init_name?=${os}-${os_profile}
+image?=rpi-hwup-image
 init_build_env?=${sources_dir}/${distro}/${init_name}-init-build-env
 
