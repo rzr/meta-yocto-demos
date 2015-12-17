2e6d1cfb3662bac0ea3798724210377afc7ed706
Philippe Coval <philippe.coval@osg.samsung.com>
2015-12-17 01:28:22 +0000

    wip: mod: rules/config.mk (sandbox/pcoval/raspberrypi)
    Change-Id: I711e12c5575ac485a534c6e1b0c7f3cddbb83af5
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/config.mk
+++ rules/config.mk
@@ -2,6 +2,20 @@
 # Author: Philippe Coval <philippe.coval@osg.samsung.com>
 # ex: set tabstop=4 noexpandtab:
 
+MACHINE?=raspberrypi2
+image?=tizen-oic-image
+bsp=raspberrypi
+
+repo_branch?=sandbox/pcoval/${bsp}
+repo_branch?=machine/${MACHINE}
+branch?=tizen
+#image?=rpi-hwup-image
+image?=tizen-base-image
+distro?=tizen-distro
+
+project_dir?=${CURDIR}
+build_dir?=${project_dir}/build
+bblayers?=${build_dir}/conf/bblayers.conf
 
 branch?=tizen
 repo_branch?=sandbox/pcoval/oic
@@ -10,9 +24,7 @@ init_name?=${branch}-common
 MACHINE?=qemux86
 machine?=${MACHINE}
 
-image_dir?=${build_dir}/tmp/deploy/images/${machine}
+#image?=rpi-hwup-image
+image?=tizen-base-image
+distro?=tizen-distro
 image?=${init_name}-core-image-minimal
-
-conf?=${build_dir}/conf/local.conf
-init_build_env?=${sources_dir}/${distro}/${init_name}-init-build-env
-
