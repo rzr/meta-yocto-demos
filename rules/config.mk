bfc14b1081aaac220bc4e08cd7d492b6bb956431
Philippe Coval <philippe.coval@osg.samsung.com>
2015-12-17 00:02:13 +0000

    wip: mod: rules/config.mk (sandbox/pcoval/raspberrypi)
    Change-Id: I024c425dffd8a88869f8ca7a96e8d48d943b031c
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/config.mk
+++ rules/config.mk
@@ -2,13 +2,6 @@
 # Author: Philippe Coval <philippe.coval@osg.samsung.com>
 # ex: set tabstop=4 noexpandtab:
 
-SHELL?=/bin/bash
-source?=.
-user?=$(shell echo ${USER})
-version?=0.0.$(shell date -u +%Y%m%d)${user}
-
-repo?=$(shell which repo || echo ${CURDIR}/repo)
-repo_url?=https://storage.googleapis.com/git-repo-downloads/repo
 
 branch?=tizen
 repo_branch?=sandbox/pcoval/oic
@@ -17,12 +10,6 @@ init_name?=${branch}-common
 MACHINE?=qemux86
 machine?=${MACHINE}
 
-DL_DIR?=${CURDIR}/cache/download
-project_dir?=${CURDIR}
-sources_dir?=${project_dir}/sources
-repo_dir?=${project_dir}/.repo
-build_dir?=${project_dir}/build
-bblayers?=${build_dir}/conf/bblayers.conf
 image_dir?=${build_dir}/tmp/deploy/images/${machine}
 image?=${init_name}-core-image-minimal
 
