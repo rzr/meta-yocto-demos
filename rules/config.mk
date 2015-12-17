021f3fdb9ce1a2ca9d72ebf9c7a487d54fc2b865
Philippe Coval <philippe.coval@osg.samsung.com>
2015-12-17 20:08:27 +0000

    wip: mod: rules/config.mk (master)
    Change-Id: I40db1c311e400839af6febf694ac010d0f91daff
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/config.mk
+++ rules/config.mk
@@ -2,30 +2,13 @@
 # Author: Philippe Coval <philippe.coval@osg.samsung.com>
 # ex: set tabstop=4 noexpandtab:
 
-SHELL?=/bin/bash
-source?=.
-user?=$(shell echo ${USER})
-version?=0.0.$(shell date -u +%Y%m%d)${user}
-
-repo?=$(shell which repo || echo ${CURDIR}/repo)
-repo_url?=https://storage.googleapis.com/git-repo-downloads/repo
+MACHINE?=qemux86
+machine?=${MACHINE}
 
 branch?=master
 repo_branch?=${branch}
 distro?=poky
 init_name?=oe
-MACHINE?=qemux86
-machine?=${MACHINE}
-
-DL_DIR?=${CURDIR}/cache/download
-project_dir?=${CURDIR}
-sources_dir?=${project_dir}/sources
-repo_dir?=${project_dir}/.repo
-build_dir?=${project_dir}/build
-bblayers?=${build_dir}/conf/bblayers.conf
-image_dir?=${build_dir}/tmp/deploy/images/${machine}
 image?=core-image-minimal
-
-conf?=${build_dir}/conf/local.conf
 init_build_env?=${sources_dir}/${distro}/${init_name}-init-build-env
 
