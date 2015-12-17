c63de58fb2907d71ad2ef7e43a283d8a04c46e95
Philippe Coval <philippe.coval@osg.samsung.com>
2015-12-17 00:17:49 +0000

    wip: mod: rules/layers.mk (sandbox/pcoval/raspberrypi)
    Change-Id: I41b9f8966cfc1fe13b9990c663b091ab5932fc5e
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/layers.mk
+++ rules/layers.mk
@@ -3,7 +3,5 @@ rule/configure/machine: tmp-configure
 tmp-configure: ${bblayers}
 	echo "BBLAYERS += \"${sources_dir}/meta-oic\" ">> $<
 	echo "BBLAYERS_NON_REMOVABLE += \"${sources_dir}/meta-oic\" ">> $<
-	echo "BBLAYERS += \"${sources_dir}/meta-yocto-demos\" ">> $<
-	echo "BBLAYERS_NON_REMOVABLE += \"${sources_dir}/meta-yocto-demos\" ">> $<
 	touch $@
 
