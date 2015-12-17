fea318972a9cea01b00becde7446fe28129c95a6
Philippe Coval <philippe.coval@osg.samsung.com>
2015-12-17 00:17:49 +0000

    wip: mod: rules/machine/raspberrypi2/conf-bsp.mk (sandbox/pcoval/raspberrypi)
    Change-Id: I9e0ec4a8138b01f01666a97ccfb627833b5e2860
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/machine/raspberrypi2/conf-bsp.mk
+++ rules/machine/raspberrypi2/conf-bsp.mk
@@ -1,5 +1,6 @@
 bsp?=raspberrypi
 MACHINE?=${bsp}2
+machine?=${MACHINE}
 repo_branch?=sandbox/pcoval/${bsp}
 repo_branch?=machine/${MACHINE}
 branch?=tizen
@@ -9,18 +10,6 @@ distro?=tizen-distro
 bsp_dir?=${CURDIR}/sources/meta-${bsp}
 conf?=${build_dir}/conf/local.conf
 
-define BBLAYERS
-"\
-BBLAYERS += \"\
-  ${CURDIR}/sources/meta-${bsp} \
-  \"\
-BBLAYERS_NON_REMOVABLE += \"\
-  ${CURDIR}/sources/meta-${bsp} \
-  \"\
-"
-endef
-export BBLAYERS
-
 rule/configure/machine: ${done_dir}/rule/configure/machine/${machine}.done
 
 rule/configure/machine/${machine}: ${done_dir}/rule/configure/machine/${machine}/bblayers.done ${done_dir}/rule/configure/machine/${machine}/conf.done
@@ -28,8 +17,6 @@ rule/configure/machine/${machine}: ${done_dir}/rule/configure/machine/${machine}
 rule/configure/machine/${machine}/bblayers: ${bblayers}
 	echo "BBLAYERS += \" ${bsp_dir} \"" >> $<
 	echo "BBLAYERS_NON_REMOVABLE += \" ${bsp_dir} \"" >> $<
-	cp -v ${CURDIR}/machine/${MACHINE}/${<F} $<
-	mkdir -p "$@{D}" ; touch $@
 
 rule/configure/machine/${machine}/conf: ${conf}
 	sed -e 's|^MACHINE ??=.*|MACHINE ??= "raspberrypi2"|g' -i $<
