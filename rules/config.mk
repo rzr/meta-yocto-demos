ffe798428180ca1319a589dad0dc25e60783afd0
Philippe Coval <philippe.coval@osg.samsung.org>
2015-12-19 22:24:22 +0000

    wip: mod: rules/config.mk (sandbox/pcoval/tizen-oic)
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.org>

--- rules/config.mk
+++ rules/config.mk
@@ -6,10 +6,11 @@ bsp?=generic
 MACHINE?=${bsp}x86
 
 os?=tizen
+extra?=oic
 os_profile?=common
 distro?=${os}-distro
-distro_branch?=${os}
 init_name?=${os}-${os_profile}
-image_type?=core
-image?=${init_name}-${image_type}-image-minimal
+image_type?=${extra}
+image?=${os}-${image_type}-image
 init_build_env?=${sources_dir}/${distro}/${init_name}-init-build-env
+
