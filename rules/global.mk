695cb73d21ce7d3cb1719286f350a1bfb651109d
Philippe Coval <philippe.coval@osg.samsung.org>
2015-12-19 20:18:41 +0000

    wip: add: rules/global.mk (master)
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.org>

---
+++ rules/global.mk
@@ -0,0 +1,17 @@
+#! /usr/bin/make -f
+# Author: Philippe Coval <philippe.coval@osg.samsung.com>
+# ex: set tabstop=4 noexpandtab:
+
+SHELL?=/bin/bash
+source?=.
+
+repo?=$(shell which repo || echo ${CURDIR}/repo)
+repo_url?=https://storage.googleapis.com/git-repo-downloads/repo
+
+project_dir?=${CURDIR}
+sources_dir?=${project_dir}/sources
+sources_layers?=$(wildcard sources/meta-*)
+repo_dir?=${project_dir}/.repo
+build_dir?=${project_dir}/build
+bblayers?=${build_dir}/conf/bblayers.conf
+tmp_dir?=tmp
