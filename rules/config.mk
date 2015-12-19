a748597cc75d153151c368cdb54648afc0117490
Philippe Coval <philippe.coval@osg.samsung.org>
2015-12-19 20:33:44 +0000

    wip: mod: rules/config.mk (distro/tizen-distro/tizen)
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.org>

--- rules/config.mk
+++ rules/config.mk
@@ -2,14 +2,18 @@
 # Author: Philippe Coval <philippe.coval@osg.samsung.com>
 # ex: set tabstop=4 noexpandtab:
 
-MACHINE?=qemux86
+bsp?=generic
+MACHINE?=${bsp}x86
 machine?=${MACHINE}
 
-distro?=poky
-distro_branch?=dizzy
+os?=tizen
+os_profile?=common
+distro?=${os}-distro
+distro_branch?=${os}
 branch?=distro/${distro}/${distro_branch}
 repo_branch?=${branch}
-init_name?=oe
-image?=core-image-minimal
+init_name?=${os}-${os_profile}
+image_type?=core
+image?=${init_name}-${image_type}-image-minimal
 init_build_env?=${sources_dir}/${distro}/${init_name}-init-build-env
 
