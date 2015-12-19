af3db548db2448a9029a72cafd9dfc546157c38e
Philippe Coval <philippe.coval@osg.samsung.org>
2015-12-19 20:46:24 +0000

    wip: mod: rules/config.mk (distro/tizen-distro/tizen)
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.org>

--- rules/config.mk
+++ rules/config.mk
@@ -4,16 +4,12 @@
 
 bsp?=generic
 MACHINE?=${bsp}x86
-machine?=${MACHINE}
 
 os?=tizen
 os_profile?=common
 distro?=${os}-distro
 distro_branch?=${os}
-branch?=distro/${distro}/${distro_branch}
-repo_branch?=${branch}
 init_name?=${os}-${os_profile}
 image_type?=core
 image?=${init_name}-${image_type}-image-minimal
 init_build_env?=${sources_dir}/${distro}/${init_name}-init-build-env
-
