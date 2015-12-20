143e7e4134515f44f9999541ff37315934adcb3c
Philippe Coval <philippe.coval@osg.samsung.org>
2015-12-20 02:35:50 +0000

    wip: mod: rules/config.mk (distro/poky/master)
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.org>

--- rules/config.mk
+++ rules/config.mk
@@ -2,10 +2,12 @@
 # Author: Philippe Coval <philippe.coval@osg.samsung.com>
 # ex: set tabstop=4 noexpandtab:
 
-
 distro?=poky
 distro_branch?=master
 branch?=distro/${distro}/${distro_branch}
 init_name?=oe
-image?=core-image-minimal
+image_base?=core-image-minimal
+image?=${image_base}
 init_build_env?=${sources_dir}/${distro}/${init_name}-init-build-env
+
+SHELL=/bin/bash
