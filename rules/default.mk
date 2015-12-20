e57f3718dd37b5f72411f3bd52c1fd1d9c4d0cb0
Philippe Coval <philippe.coval@osg.samsung.org>
2015-12-20 17:23:23 +0000

    wip: mod: rules/default.mk (sandbox/pcoval/tizen-raspberrypi)
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.org>

--- rules/default.mk
+++ rules/default.mk
@@ -20,19 +20,22 @@ os?=${generic}
 os_profile?=${generic}
 distro?=poky
 distro_branch?=${os}
+
+repo?=$(shell which repo || echo ${CURDIR}/repo)
+repo_url?=https://storage.googleapis.com/git-repo-downloads/repo
 repo_branch?=${branch}
 repo_src_file?=default.xml
-repo_file?=${tmp_dir}/default.xml
+repo_file?=${repo_src_file}.tmp
 
 init_name?=${os}-${os_profile}
 init_build_env?=${sources_dir}/${distro}/${init_name}-init-build-env
-image_type?=core
-image?=${image_type}-image-minimal
+image_base?=core-image-minimal
+image?=${image_base}
 
 project_dir?=${CURDIR}
-build_dir?=${project_dir}/build
+cache_dir?=${project_dir}/
+build_dir?=${cache_dir}/build
 bblayers?=${build_dir}/conf/bblayers.conf
+bsp_relative_dir?=../..
 image_dir?=${build_dir}/tmp/deploy/images/${machine}
 conf?=${build_dir}/conf/local.conf
-
-
