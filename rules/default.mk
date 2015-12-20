8f32366bb99f7ded991b29a09052bd7b16479d1e
Philippe Coval <philippe.coval@osg.samsung.org>
2015-12-20 02:35:50 +0000

    wip: mod: rules/default.mk (distro/poky/master)
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.org>

--- rules/default.mk
+++ rules/default.mk
@@ -22,16 +22,17 @@ distro?=poky
 distro_branch?=${os}
 repo_branch?=${branch}
 repo_src_file?=default.xml
-repo_file?=${tmp_dir}/default.xml
+repo_file?=${repo_src_file}.tmp
 
 init_name?=${os}-${os_profile}
 init_build_env?=${sources_dir}/${distro}/${init_name}-init-build-env
-image_type?=core
-image?=${image_type}-image-minimal
+image_base?=core-image-minimal
+image?=${image_base}
 
 project_dir?=${CURDIR}
 build_dir?=${project_dir}/build
 bblayers?=${build_dir}/conf/bblayers.conf
+bsp_relative_dir?=../..
 image_dir?=${build_dir}/tmp/deploy/images/${machine}
 conf?=${build_dir}/conf/local.conf
 
