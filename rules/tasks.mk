4238b1fd0902f48f5c639d78c4b7a2f1ac989847
Philippe Coval <philippe.coval@osg.samsung.org>
2015-12-20 17:23:23 +0000

    wip: mod: rules/tasks.mk (sandbox/pcoval/tizen-raspberrypi)
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.org>

--- rules/tasks.mk
+++ rules/tasks.mk
@@ -4,6 +4,7 @@
 SELF?=${CURDIR}/rules/tasks.mk
 
 default: rule/help
+	date
 
 rule/help: ${SELF}
 	@echo "# Usage:"
@@ -13,10 +14,13 @@ rule/help: ${SELF}
 	@echo "# MACHINE=${MACHINE}"
 	@echo "# USER=${USER}"
 	@echo "# DL_DIR=${DL_DIR}"
-	@echo "# image_dir=${image_dir}"
 	@echo "# sources_dir=${sources_dir}"
+	@echo "# cache_dir=${cache_dir}"
+	@echo "# build_dir=${build_dir}"
+	@echo "# image_dir=${image_dir}"
 	@echo "# distro=${distro}"
 	@echo "# conf=${conf}"
+	@echo "# image=${image}"
 	@echo ""
 	@echo "# Existing rules :"
 	@grep -o -e '^[^# 	]*:' $< | grep -v '\$$'
@@ -26,15 +30,16 @@ ${tmp_dir}/%.done: %
 	touch $@
 
 rule/all: ${tmp_dir}/rule/setup.done
-	${MAKE} rule/configure rule/env/image
+	${MAKE} rule/configure rule/path rule/env/image
 
 ${repo_dir}: ${repo} ${repo_file}
 	mkdir -p $@ && cd $@/.. && ${repo} init -q -u . -b ${repo_branch}
 
-${sources_dir}/${distro}: rule/sync
+${sources_dir}/${distro}: rule/repo/sync
+	ls -l ${@}/conf/combo-layer.conf
 
-#${conf} ${bblayers}: ${tmp_dir}/rule/setup.done
-#	@make rule/env/help
+${conf} ${bblayers}: ${tmp_dir}/rule/setup.done
+	@make rule/env/help
 
 ${sources_dir}: ${repo_dir}
 	cd $</.. && ${repo} sync
@@ -45,33 +50,51 @@ ${repo}:
 	chmod u+rx $@
 
 ${init_build_env}: ${sources_dir}
+	ls -l ${@D}
 
+# workaround a /bin/sh behaviour
 ${build_dir}: ${init_build_env}
-	cd ${<D} && ${source} ${<} ${build_dir} && pwd
-	ls ${build_dir} || ln -fs ${<D}/build ${build_dir}
-	ls ${build_dir}/conf
+	mkdir -p ${@D}
+	cd ${<D} && ${source} ${<} ${@}
+	ls ${<D}/build || ln -fs ${@} ${<D}/build
+	ls ${@} || ln -fs ${<D}/build ${@}
+	ls ${@}/conf
 
 rule/sync: ${repo_dir} ${repo} 
 	cd $</.. && time ${repo} sync && ${repo} list
 
 rule/init_build_env: ${init_build_env}
+	ls $<
 
 rule/bblayers: ${bblayers}
+	ls $<
 
 rule/sources: ${sources_dir}
+	ls $<
 
 rule/build: ${build_dir}
+	ls $<
 
 rule/setup: rule/build
+	date
 
 rule/configure/layer/%: % ${bblayers}
 	echo "BBLAYERS += \"${CURDIR}/${<}\"" >> ${bblayers}
 	echo "BBLAYERS_NON_REMOVABLE += \"${CURDIR}/${<}\"" >> ${bblayers}
 
 rule/configure/layer/.:
+	date
 
 rule/configure: ${sources_dir} rule/configure/machine rule/configure/downloads
 	for dir in . ${sources_layers} ; do make $@/layer/$${dir} ; done
+
+# BSPDIR := "${@os.path.abspath(os.path.dirname(d.getVar('FILE', True)) + '/../..')}"
+rule/path: ${bblayers}
+	test -r "${<}.orig" || cp -av "${<}" "${<}.orig"
+	echo "BSPDIR := \"\$${@os.path.abspath(os.path.dirname(d.getVar('FILE', True)) + '/${bsp_relative_dir}')}\"" > "${<}.tmp"
+	sed -e "s|${CURDIR}|\$${BSPDIR}|g" < "${<}.orig" >> "${<}.tmp"
+	mv "${<}.tmp" "${<}"
+	grep "${bsp_relative_dir}" ${bblayers}
 
 rule/configure/machine: ${conf}
 	sed -e "s|^MACHINE ??=.*|MACHINE ??= \"${MACHINE}\"|g" -i $<
@@ -80,6 +103,7 @@ rule/configure/downloads: ${build_dir}
 	[ "" = "${DL_DIR}" ] || ln -fs "${DL_DIR}" $</downloads
 
 rule/conf: ${conf}
+	@ls -l $<
 
 rule/image: ${build_dir}
 	cd $< && time bitbake "${image}"
@@ -105,16 +129,21 @@ rule/purge: rule/distclean
 	rm -rf -- ${repo_dir} build*
 
 all: rule/all
+	date
 
 rebuild: rule/purge rule/all
+	date
 
-log/%:
-	mkdir -p ${tmp_dir}
-	script -c "${MAKE} ${@F}" ${logfile}
+log/%: ${tmp_dir}
+	script -e -c "${MAKE} ${@F}" ${logfile}
+
+${tmp_dir}:
+	mkdir -p $@
 
 rule/setup/repo: ${repo_file}
+	date
 
 ${repo_file}: ${repo_src_file}
 	mkdir -p ${@D}
-	sed -e "s|<project name=\"${project_name}\" path=\"sources/${project_name}\" remote=\"${remote}\" revision=\".*\"/>|<project name=\"${project_name}\" path=\"sources/${project_name}\" remote=\"${remote}\" revision=\"${branch}\"/>|g" < $< > $@.tmp && mv $@.tmp $@
-
+	sed -e "s|<project name=\"${project_name}\" path=\"sources/${project_name}\" remote=\"${remote}\" revision=\".*\"/>|<project name=\"${project_name}\" path=\"sources/${project_name}\" remote=\"${remote}\" revision=\"${branch}\"/>|g" < $< > $@
+	cp -av $@ $<
