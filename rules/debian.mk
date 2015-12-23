99377119184fe47951e6f59124e4bc44fc2ebf0b
Philippe Coval <philippe.coval@osg.samsung.org>
2015-12-23 21:17:22 +0000

    wip: mod: rules/debian.mk (meta/poky/master)
    Change-Id: I6b72beb5198468dee4a04ab5b042c5a72a9aa83b
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.org>

--- rules/debian.mk
+++ rules/debian.mk
@@ -1,8 +1,8 @@
 rule/setup/debian:
 	   sudo apt-get install \
- gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential \
- chrpath libsdl1.2-dev xterm libattr1-dev binutils-gold quilt cpio libwayland-dev ccache \
- curl
+	gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential \
+   chrpath libsdl1.2-dev xterm libattr1-dev binutils-gold quilt cpio libwayland-dev ccache \
+	curl
 
 
 email?=${USER}@localhost
