85a50ec77a6658c00b26a9c458af184b9688d3b2
Philippe Coval <philippe.coval@osg.samsung.org>
2015-12-23 20:54:15 +0000

    wip: add: rules/default.mk (master)
    Change-Id: I6262e8fc697036f5cb087ee9c830ea21a9a5a078
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.org>

---
+++ rules/default.mk
@@ -0,0 +1,50 @@
+#! /usr/bin/make -f
+# Author: Philippe Coval <philippe.coval@osg.samsung.com>
+# ex: set tabstop=4 noexpandtab:
+
+project_name?=meta-yocto-demos
+remote?=tizenteam
+user?=$(shell echo ${USER})
+log_file?=tmp/build.log
+version?=0.0.$(shell date -u +%Y%m%d)${user}
+email?=${USER}@localhost
+name=${USER}
+
+branch?=$(shell git rev-parse --abbrev-ref HEAD)
+branch?=master
+
+generic?=generic
+bsp?=qemu
+MACHINE?=${bsp}x86
+machine?=${MACHINE}
+
+os?=${generic}
+os_profile?=${generic}
+distro?=poky
+distro_branch?=${os}
+
+repo?=$(shell which repo || echo ${CURDIR}/repo)
+repo_url?=https://storage.googleapis.com/git-repo-downloads/repo
+repo_branch?=${branch}
+repo_src_file?=default.xml
+repo_file?=local.xml
+
+init_name?=${os}-${os_profile}
+init_build_env?=${sources_dir}/${distro}/${init_name}-init-build-env
+image_type?=core
+image_base?=${image_type}-image-minimal
+image?=${image_base}
+images?=${image}
+
+project_dir?=${CURDIR}
+cache_dir?=${project_dir}/
+build_dir?=${cache_dir}build
+bblayers_file?=${build_dir}/conf/bblayers.conf
+bsp_relative_dir?=../..
+image_dir?=${build_dir}/tmp/deploy/images/${machine}
+conf_file?=${build_dir}/conf/local.conf
+
+images?=${image}
+
+local_name=localhost
+local_url?=file://${CURDIR}/
