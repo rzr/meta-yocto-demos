bd3d802779c0ea9ba8318e7154d7d19042e4e5ac
Philippe Coval <philippe.coval@osg.samsung.org>
2015-12-23 22:21:14 +0000

    wip: mod: rules/config.mk (meta/poky/dizzy)
    Change-Id: I56644be4ee8ade9ca9e1f11474e94abcef25c933
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.org>

--- rules/config.mk
+++ rules/config.mk
@@ -3,9 +3,3 @@
 # ex: set tabstop=4 noexpandtab:
 
 
-distro?=poky
-distro_branch?=dizzy
-branch?=distro/${distro}/${distro_branch}
-init_name?=oe
-image?=core-image-minimal
-init_build_env?=${sources_dir}/${distro}/${init_name}-init-build-env
