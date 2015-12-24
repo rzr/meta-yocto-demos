458fd6e2c00b212ead75a2a1c4b9446e93df7dd8
Philippe Coval <philippe.coval@osg.samsung.org>
2015-12-24 01:32:31 +0000

    wip: mod: rules/config.mk (meta/intel/dizzy)
    Change-Id: I574ed1f6f5510f88a352725a4810a1dbc1d79450
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.org>

--- rules/config.mk
+++ rules/config.mk
@@ -2,4 +2,44 @@
 # Author: Philippe Coval <philippe.coval@osg.samsung.com>
 # ex: set tabstop=4 noexpandtab:
 
+root_bsp=intel
+bsp?=fri2
+MACHINE?=${bsp}-noemgd
 
+os?=tizen
+os_profile?=micro
+distro?=${os}-distro
+distro_branch?=${os}
+extra?=oic
+init_name?=${os}-common
+base_image?=${os}-${os_profile}-image
+image?=tizen-core-image-minimal
+init_build_env?=${sources_dir}/${distro}/${init_name}-init-build-env
+
+SHELL=/bin/bash
+
+repo_dir?=${project_dir}/.repo2
+build_dir=${project_dir}/build-${MACHINE}
+
+rule/bitbake/cleanall/%: 
+	${MAKE} rule/env/bitbake ARGS="-c cleanall ${@F}"
+
+rule/bitbake/clean/%: 
+	${MAKE} rule/env/bitbake ARGS="-c clean ${@F}"
+
+rule/bitbake/task/%: 
+	${MAKE} rule/env/bitbake ARGS="${@F}"
+
+rule/bitbake/rebuild/%: rule/bitbake/cleanall/% rule/bitbake/task/%
+	date
+
+rule/test:
+	make rule/bitbake/rebuild/weston MACHINE=raspberrypi2
+	make rule/bitbake/rebuild/weston MACHINE=genericx86
+	make rule/bitbake/rebuild/weston MACHINE=genericx86-64
+
+
+rule/configure/conf: ${conf_file}
+	ls $<
+
+sources_layers+=sources/meta-${root_bsp}/meta-${bsp}
