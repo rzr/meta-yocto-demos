552ea43b96a2d9e97dd9257ee55e3e976ea3f60e
Philippe Coval <philippe.coval@osg.samsung.org>
2015-12-24 01:47:58 +0000

    wip: mod: rules/config.mk (meta/amlogic/master)
    Change-Id: I64d8325a5dfdfa8c892266a0466c2f499c65cc02
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.org>

--- rules/config.mk
+++ rules/config.mk
@@ -5,20 +5,13 @@
 bsp?=amlogic
 MACHINE?=odroidc1
 
-os?=tizen
-os_profile?=micro
-distro?=${os}-distro
-distro_branch?=${os}
-extra?=oic
-init_name?=${os}-common
-base_image?=${os}-${os_profile}-image
+os?=poky
+os_profile?=master
+distro?=${os}
+init_name?=oe
+base_image?=core-image-minimal
 image?=${base_image}
 init_build_env?=${sources_dir}/${distro}/${init_name}-init-build-env
-
-rule/configure/conf: ${conf_file}
-	ls $<
-#	echo "DISTRO_FEATURES_remove = \" x11 wayland \"" >> $<
-
 cache_dir?=${CURDIR}/
 build_dir?=${cache_dir}build
 
@@ -26,4 +19,5 @@ build_dir?=${cache_dir}build
 conf_file?=${build_dir}/conf/local.conf
 
 rule/configure/conf: ${conf_file}
-	ls -l $^
+	ls $<
+#	echo "DISTRO_FEATURES_remove = \" x11 wayland \"" >> $<
