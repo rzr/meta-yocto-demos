7d5197c7ade0fb1a0cf68cd6a903042d018edbc7
Philippe Coval <philippe.coval@osg.samsung.org>
2015-12-25 00:15:42 +0000

    wip: mod: rules/tasks.mk (sandbox/pcoval/meta/fri2/tizen)
    Change-Id: I5c24f81b9ca004afc8b0c55530af9b9a4ecf2c02
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.org>

--- rules/tasks.mk
+++ rules/tasks.mk
@@ -3,7 +3,7 @@
 # ex: set tabstop=4 noexpandtab:
 SELF?=${CURDIR}/rules/tasks.mk
 
-default: rule/help
+default: rule/help rule/all
 	date
 
 rule/help: ${SELF}
@@ -11,8 +11,12 @@ rule/help: ${SELF}
 	@echo "# make rule/all"
 	@echo ""
 	@echo "# Configuration / Environement:"
+	@echo "# project_name=${project_name}"
+	@echo "# branch=${branch}"
 	@echo "# MACHINE=${MACHINE}"
 	@echo "# USER=${USER}"
+	@echo "# email=${email}"
+	@echo "# version=${version}"
 	@echo "# DL_DIR=${DL_DIR}"
 	@echo "# sources_dir=${sources_dir}"
 	@echo "# cache_dir=${cache_dir}"
@@ -29,72 +33,63 @@ ${tmp_dir}/%.done: %
 	mkdir -p ${@D}
 	touch $@
 
-rule/all: ${tmp_dir}/rule/setup.done
-	${MAKE} rule/configure rule/path rule/env/image
+rule/make/error:
+	$(error "error: ${ARG}")
 
-${repo_dir}: ${repo_file} ${repo}
-	mkdir -p $@ && cd $@/.. && ${repo} init -q -u ${local_url} -b ${repo_branch} -m $<
+rule/make/rule/%:
+	$(info "sub make to rescan files")
+	${MAKE} ${@F} ${ARGS}
 
-rule/setup/repo: ${repo_file}
+rules/config.mk:
+	@echo "#distro?=TODO" > $@
+
+rule/all: ${tmp_dir}/configure.done rule/env/image
+
+
+rule/repo/%: ${repo_dir}/.repo ${repo}
+	cd ${<D} && time ${repo} ${@F} && ${repo} list
+
+rule/repo/sync: ${repo_dir}/.repo ${repo}
+	cd ${<D} && time ${repo} sync && ${repo} list
+
+rule/repo/setup: ${repo_file} rule/repo/init rule/repo/sync
 	date
-
-${repo_file}: ${repo_src_file}
-	mkdir -p ${@D}
-	cat $< > $@
-	grep ${local_url} $@ \
-	|| sed -e "s|<manifest>|<manifest>\n  <remote fetch=\"${local_url}/../\" name=\"${local_name}\"/>|g" < $< > $@
-	sed -e "s|<project name=\"${project_name}\" path=\"sources/${project_name}\" remote=\"${remote}\" revision=\".*\"/>|<project name=\"${project_name}\" path=\"sources/${project_name}\" remote=\"${local_name}\" revision=\"${branch}\"/>|g" -i $@
-	git add $@ 
-	git commit -sam "WIP: dont push"
-#	cp -av $@ $<
-
-rule/repo/file: ${repo_file}
-	grep ${local_name} $<
-
-rule/images:
-	for machine in ${machines} ; do \
-	for image in ${images} ; do \
-	make rule/all MACHINE=$${machine} image=$${image} \
-	|| echo "$${image}/$${machine}" >> tmp/fail.log ; \
-	done ; \
-	done ;
-
-rule/repo/%: ${repo_dir} ${repo} 
-	cd $</.. && time ${repo} ${@F} && ${repo} list
-
-rule/setup/repo: ${repo_file}
-	date
-rule/error:
-	$(error "${ARG}")
-
-${sources_dir}/${distro}: rule/repo/sync
-	ls $@ || make rule/error ARG="TODO: setup distro in rule/config.mk"
-	@ls -l ${@}/meta
-
-${conf_file} ${bblayers_file}: ${tmp_dir}/rule/setup.done
-	@make rule/env/help
-
-${sources_dir}: ${repo_dir}
-	cd $</.. && ${repo} sync
 
 ${repo}:
 	mkdir -p ${@D}
 	wget -nc -O $@ ${repo_url}
 	chmod u+rx $@
 
+${sources_dir}/${project_name}: ${sources_dir}
+	ln -fs .. ${sources_dir}/${project_name}
+
+${repo_dir}/.repo: ${repo_file} ${repo}
+	mkdir -p $@ && cd $@/.. \
+	&& ${repo} init -q -u ${local_url} -b ${branch} -m ${<F}
+
 rule/repo: ${repo}
-	${<} --help
+	ls ${<}
+	@${<} --help
 
 rule/repo/dir: ${repo_dir}
 	du -hsc $<
 
+${sources_dir}/${distro}: rules/config.mk ${tmp_dir}/rule/repo/sync.done
+	@ls -l ${@}/meta || make rule/make/error ARG="Please set distro var in rules/config.mk"
+
+${sources_dir}: rule/repo/sync
+	ls -l ${@}
+
+${conf_file}: ${tmp_dir}/rule/setup.done
+	@make rule/env/help
+
 ${init_build_env}: ${sources_dir}/${distro}
 	ls -l ${@D}
 
-${build_dir}: ${init_build_env}
-	mkdir -p ${@D}
-	cd ${<D} && ${source} ${<} ${@}
-	ls ${<D}/${@F} || ln -fs ${@} ${<D}/${@F}
+${build_dir}: ${init_build_env} ${sources_dir}/${project_name}
+	mkdir -p ${build_dir}
+	cd ${<D} && ${source} ${<} ${build_dir}
+	ls ${<D}/${@F} || grep SHELL rules/*.mk && ln -fs ${@} ${<D}/${@F}
 	ls ${@} || ln -fs ${<D}/${@F} ${@}
 	ls ${@}/conf
 	$(info "workaround a /bin/sh behaviour make sure to set SHELL=/bin/bash")
@@ -106,7 +101,7 @@ rule/bblayers: ${bblayers}
 	ls $<
 
 rule/sources: ${sources_dir}
-	ls $<
+	ls $< | wc -l
 
 rule/build: ${build_dir}
 	ls $<
@@ -114,23 +109,8 @@ rule/build: ${build_dir}
 rule/setup: rule/build
 	date
 
-rule/configure/layer/%: % ${bblayers}
-	echo "BBLAYERS += \"${CURDIR}/${<}\"" >> ${bblayers}
-	echo "BBLAYERS_NON_REMOVABLE += \"${CURDIR}/${<}\"" >> ${bblayers}
-
-rule/configure/layer/.:
-	date
-
-rule/configure: ${sources_dir} rule/configure/conf rule/configure/machine rule/configure/downloads
-	for dir in . ${sources_layers} ; do make $@/layer/$${dir} ; done
-
-# RELATIVE_DIR := "${@os.path.abspath(os.path.dirname(d.getVar('FILE', True)) + '/../..')}"
-rule/path: ${bblayers}
-	test -r "${<}.orig" || cp -av "${<}" "${<}.orig"
-	echo "RELATIVE_DIR := \"\$${@os.path.abspath(os.path.dirname(d.getVar('FILE', True)) + '/${bsp_relative_dir}')}\"" > "${<}.tmp"
-	sed -e "s|${CURDIR}|\$${RELATIVE_DIR}|g" < "${<}.orig" >> "${<}.tmp"
-	mv "${<}.tmp" "${<}"
-	grep "${bsp_relative_dir}" ${bblayers}
+rule/configure/conf: ${conf_file}
+	grep -i MACHINE ${conf_file}
 
 rule/configure/machine: ${conf_file}
 	sed -e "s|^MACHINE ??=.*|MACHINE ??= \"${MACHINE}\"|g" -i $<
@@ -138,21 +118,76 @@ rule/configure/machine: ${conf_file}
 rule/configure/downloads: ${build_dir}
 	[ "" = "${DL_DIR}" ] || ln -fs "${DL_DIR}" $</downloads
 
+sub-rule/layer/%: %
+	ls -l ${bblayers_file}
+	echo "BBLAYERS += \"${CURDIR}/${<}\"" >> ${bblayers_file}
+	echo "BBLAYERS_NON_REMOVABLE += \"${CURDIR}/${<}\"" >> ${bblayers_file}
+
+sub-rule/layer/.: ${sources_dir}/${project_name}
+	ls -l ${bblayers_file}
+	ls -l $<
+
+${bblayers_file}: ${tmp_dir}/configure-layers.done
+	grep BBLAYERS $@ | wc -l
+
+sub-configure-layers/path: ${bblayers_file}.orig
+	echo "RELATIVE_DIR := \"\$${@os.path.abspath(os.path.dirname(d.getVar('FILE', True)) + '/${project_relative_dir}')}\"" > "${<}.tmp"
+	sed -e "s|${CURDIR}|\$${RELATIVE_DIR}|g" < "${<}" >> "${<}.tmp"
+	mv "${<}.tmp" "${bblayers_file}"
+	grep "RELATIVE_DIR" ${bblayers_file}
+#	# RELATIVE_DIR := "${@os.path.abspath(os.path.dirname(d.getVar('FILE', True)) + '/../..')}"
+
+${bblayers_file}.orig:
+	cp -av "${bblayers_file}" "${@}"
+
+sub-configure-layers: ${conf_file}
+	$(info "log: sources_layers=${sources_layers}")
+	for dir in ${sources_layers} ; do make sub-rule/layer/$${dir} ; done
+	${MAKE} $@/path
+
+sub-configure-rescan: ${sources_dir} rule/configure/machine ${tmp_dir}/sub-configure-layers.done ${tmp_dir}/rule/configure/downloads.done
+	$(info "log: sources_layers=${sources_layers}")
+	echo "# remember dont call $@ direcly but configure"
+
+configure: rule/repo/sync ${tmp_dir}/rule/make/rule/sub-configure-rescan.done
+	ls -l ${bblayers_file}
+
 rule/conf: ${conf_file}
 	@ls -l $<
 
 rule/image: ${build_dir}
 	cd $< && time bitbake "${image}"
 
+rule/images: ${tmp_dir}
+	for machine in ${machines} ; do \
+	for image in ${images} ; do \
+	make rule/all MACHINE=$${machine} image=$${image} \
+	|| echo "$${image}/$${machine}" >> ${tmp_dir}/fail.log ; \
+	done ; \
+	done ;
+
 rule/env/%: ${init_build_env}
-	cd ${<D} && ${source} ${<} ${build_dir} \
-	&& make -C ${CURDIR} rule/${@F} ARGS="${ARGS}"
+	cd ${<D}  \
+ && ${source} ${<} ${build_dir} \
+ && make -C ${CURDIR} rule/${@F} ARGS="${ARGS}"
 
 rule/bitbake: ${build_dir}
 	cd $< && time ${@F} ${ARGS}
 
+rule/bitbake/cleanall/%:
+	${MAKE} rule/env/bitbake ARGS="-c cleanall ${@F}"
+
+rule/bitbake/clean/%:
+	${MAKE} rule/env/bitbake ARGS="-c clean ${@F}"
+
+rule/bitbake/task/%:
+	${MAKE} rule/env/bitbake ARGS="${@F}"
+
+rule/bitbake/rebuild/%: rule/bitbake/cleanall/% rule/bitbake/task/%
+	date
+
 rule/clean:
-	rm -rfv ${build_dir}/tmp *~ .#* 
+	rm -rfv ${build_dir}/tmp *~ .#*
 	@echo "# make rule/{cleanall,distclean,purge} to clean more"
 
 rule/cleanall: rule/clean
@@ -165,7 +200,7 @@ rule/distro: ${sources_dir}/${distro}
 	grep ${<F} rules/*.mk
 
 rule/purge: rule/distclean
-	rm -rf -- ${repo_dir} build*
+	rm -rf -- ${repo_dir}/.repo build*
 
 all: rule/all
 	date
