e507c27f6f0ffc9f3f228af4af2d896580213b6f
Philippe Coval <philippe.coval@osg.samsung.org>
2015-12-31 15:16:21 +0000

    wip: mod: rules/config.mk (sandbox/pcoval/meta/fri2/tizen)
    Change-Id: I155a9d70b582e851e01ac52f8e45735ad06b9f85
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.org>

--- rules/config.mk
+++ rules/config.mk
@@ -4,7 +4,6 @@
 
 SHELL=/bin/bash
 V=1
-
 root_bsp=intel
 bsp?=fri2
 MACHINE?=${bsp}-noemgd
@@ -15,9 +14,15 @@ distro?=${os}-distro
 extra?=
 init_name?=${os}-${os_profile}
 base_image?=core-image-minimal
-image?=${os}-${base_image} \
+image?=${os}-${base_image}
+images?=${image} \
  ${os}-${os_profile}-core-image-minimal \
  ${os}-${os_profile}-core-image-crosswalk
 
-sources_layers?=$(sort $(wildcard sources/meta-*))
-sources_layers+=sources/meta-${root_bsp}/meta-${bsp}
+sources_layers_conf?=$(sort $(wildcard sources/meta-*/conf/layer.conf))
+sources_layers_dirs?=$(dir ${sources_layers_conf})
+sources_layers?=$(shell dirname $(sources_layers_dirs))
+
+
+sources_layers+=sources/meta-${root_bsp}/meta-${bsp} \
+ meta-${root_bsp}/meta-${bsp}
