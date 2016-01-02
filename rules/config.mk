6145f247b7e2bed257716ad48802a8738eea87bd
Philippe Coval <philippe.coval@osg.samsung.com>
2016-01-02 17:45:53 +0100

    wip: mod: rules/config.mk (tizen)
    Change-Id: I5e7c498141507e61d39f850683f8a7137b38a39e
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/config.mk
+++ rules/config.mk
@@ -8,12 +8,22 @@ root_bsp=generic
 bsp?=${root_bsp}
 MACHINE?=${bsp}x86-64
 
-os?=oe
-os_profile?=
-distro?=poky
+machines?=${machine} \
+ ${bsp}x86
+
+os?=tizen
+os_profile?=common
+distro?=${os}-distro
 extra?=
-init_name?=${os}
+init_name?=${os}-${os_profile}
 base_image?=core-image-minimal
 image?=${base_image}
-images?=${image} ${image}-dev
 
+images?=${image} ${image}-dev \
+ ${os}-${os_profile}-${base_image} \
+ ${os}-${os_profile}-${base_image}-dev \
+ tizen-common-core-image-crosswalk \
+ tizen-common-core-image-crosswalk-dev
+
+
+sources_layers_conf+=
