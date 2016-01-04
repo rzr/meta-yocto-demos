131efc4637d303a4722675f16d8b2db563febbcf
Philippe Coval <philippe.coval@osg.samsung.com>
2016-01-04 12:41:07 +0100

    wip: mod: rules/90-overides.mk (sandbox/pcoval/wip)
    Change-Id: If4ca231cc92a3babac59ba6702219b52c4de8712
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/90-overides.mk
+++ rules/90-overides.mk
@@ -3,7 +3,7 @@
 # ex: set tabstop=4 noexpandtab:
 
 rule/overide/rule/configure-conf: rule/configure-conf
-	ls -l ${conf_file}
+
 
 rule/overide/rule/all: rule/all rule/bitbake/task/iotivity rule/bitbake/task/iotivity-simple-client rule/bitbake/task/linux-yocto
 	@echo "TODO: rule/bitbake/task/iotivity-sensorboard"
