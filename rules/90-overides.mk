98614f4a4ea29317642e46e7295707fda923b145
Philippe Coval <philippe.coval@osg.samsung.com>
2016-01-04 09:22:17 +0100

    wip: add: rules/90-overides.mk (sandbox/pcoval/wip)
    Change-Id: I292178f69b797b9b2eec83b4cb2a7979b7ab6184
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ rules/90-overides.mk
@@ -0,0 +1,6 @@
+#! /usr/bin/make -f
+# Author: Philippe Coval <philippe.coval@osg.samsung.com>
+# ex: set tabstop=4 noexpandtab:
+
+rule/overide/rule/configure-conf: rule/configure-conf
+	ls -l ${conf_file}
