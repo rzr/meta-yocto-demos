a7cca0ed83312473c901be8ef6ace9ef62bcc9fa
Philippe Coval <philippe.coval@osg.samsung.com>
2016-01-08 15:01:57 +0100

    wip: mod: rules/10-config.mk (tizen)
    Change-Id: Ibacc191f6f3ea1fc01458c577032cf433c64cfa9
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/10-config.mk
+++ rules/10-config.mk
@@ -7,17 +7,21 @@ V=1
 root_bsp=generic
 bsp?=${root_bsp}
 MACHINE?=${bsp}x86
-
-os?=oe
-os_profile?=
-distro?=poky
+machine?=${MACHINE}
+machines?=${machine} ${machine}-64
+os?=tizen
+os_profile?=common
+distro?=${os}-distro
 extra?=
-init_name?=${os}
+init_name?=${os}-${os_profile}
 base_image?=core-image-minimal
-image?=${base_image}
-images?=${image} ${image}-dev
+image?=oic-core-image
+images?=${base_image} \
+ ${image} ${image}-dev \
+ ${os}-${os_profile}-${base_image} \
+ tizen-common-core-image-crosswalk \
+ tizen-common-core-image-crosswalk-dev
 
 sources_layers_conf?=$(sort $(wildcard sources/meta-*/conf/layer.conf))
 
 sources_layers_conf+=
-
