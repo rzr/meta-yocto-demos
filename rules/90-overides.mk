40ed2daf253072b7a77da168143c4a9781623c20
Philippe Coval <philippe.coval@osg.samsung.com>
2016-01-12 18:10:38 +0100

    WIP: mod: rules/90-overides.mk (sandbox/pcoval/wip)
    Change-Id: I9d86670d16aab2d1da1f1978615e810ec0167719
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/90-overides.mk
+++ rules/90-overides.mk
@@ -4,7 +4,7 @@
 
 rule/overide/rule/configure-conf: rule/configure-conf
 	install -d sources/tizen-distro/meta-tizen/meta-tizen-micro/meta-raspberrypi/recipes-image/
-	mv \
+	-mv -fv \
 	 sources/tizen-distro/meta-tizen/meta-tizen-micro/recipes-image/raspberrypi2/rpi-hwup-image-tizen-micro.bb \
 	 sources/tizen-distro/meta-tizen/meta-tizen-micro/meta-raspberrypi/recipes-image/tizen-micro-rpi-hwup-image.bb 
 	ls -l ${conf_file}
@@ -12,7 +12,6 @@ rule/overide/rule/configure-conf: rule/configure-conf
 rule/overide/rule/all: rule/all \
   rule/bitbake/task/iotivity \
   rule/bitbake/task/iotivity-simple-client \
-  rule/bitbake/task/linux-yocto \
   rule/bitbake/task/iotivity-example \
   rule/bitbake/task/iotivity-sensorboard
 	@echo "success: $@" 
