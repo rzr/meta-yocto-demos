5a14d427ed9e52eb4d943f89259944327adfba54
Philippe Coval <philippe.coval@osg.samsung.com>
2016-01-13 11:05:01 +0100

    config: build tizen-distro images
    Change-Id: I8c9fda1f275a457860e3c7d6f1ddcfa0b65ac928
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/10-config.mk
+++ rules/10-config.mk
@@ -7,15 +7,23 @@ V=1
 root_bsp=generic
 bsp?=${root_bsp}
 MACHINE?=${bsp}x86
+machines?=${machine} \
+ ${machine}-64
 
-os?=oe
-os_profile?=
-distro?=poky
+os?=tizen
+os_profile?=common
+distro?=${os}-distro
 extra?=
-init_name?=${os}
+init_name?=${os}-${os_profile}
 base_image?=core-image-minimal
 image?=${base_image}
 images?=${image} ${image}-dev
+
+images?=${image} ${image}-dev \
+ ${os}-${os_profile}-${base_image} \
+ ${os}-${os_profile}-${base_image}-dev \
+ ${os}-${os_profile}-core-image-crosswalk \
+ ${os}-${os_profile}-core-image-crosswalk-dev
 
 sources_layers_conf?=$(sort $(wildcard sources/meta-*/conf/layer.conf))
 
