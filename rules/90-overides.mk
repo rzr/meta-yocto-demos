110a4392b3ecd688962d0ba74b9e96e46c921ad7
Philippe Coval <philippe.coval@osg.samsung.com>
2016-01-13 18:32:30 +0100

    WIP: mod: rules/90-overides.mk (sandbox/pcoval/wip)
    Change-Id: Idc255d42e8d167210dca14f1a0a086e8e52a39dc
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/90-overides.mk
+++ rules/90-overides.mk
@@ -2,12 +2,26 @@
 # Author: Philippe Coval <philippe.coval@osg.samsung.com>
 # ex: set tabstop=4 noexpandtab:
 
-rule/overide/rule/configure-conf: rule/configure-conf
+rule/machine/${MACHINE}/configure:
 	install -d sources/tizen-distro/meta-tizen/meta-tizen-micro/meta-raspberrypi/recipes-image/
 	-mv -fv \
 	 sources/tizen-distro/meta-tizen/meta-tizen-micro/recipes-image/raspberrypi2/rpi-hwup-image-tizen-micro.bb \
 	 sources/tizen-distro/meta-tizen/meta-tizen-micro/meta-raspberrypi/recipes-image/tizen-micro-rpi-hwup-image.bb 
+	install -d ../tmp/${CURDIR}
+	-mv -v sources/meta-50-oic/recipes-kernel ../tmp/${CURDIR}
+
+
+rule/overide/rule/configure-conf: rule/configure-conf rule/machine/${MACHINE}/configure
 	ls -l ${conf_file}
+	ls -l sources/meta-*${bsp}/conf/machine/${MACHINE}.conf
+
+
+rule/wip:
+	mv sources/tizen-distro/meta-tizen sources/tizen-distro/meta-tizen.orig
+	ln -fs \
+  /home/philippe/var/cache/url/git/ssh/review.tizen.org/scm/bb/meta-tizen/src/meta-tizen \
+  sources/tizen-distro/meta-tizen
+
 
 rule/overide/rule/all: rule/all \
   rule/bitbake/task/iotivity \
