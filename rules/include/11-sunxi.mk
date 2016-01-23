55a4095220e0631ac9f91f3a7178661178c88e7f
Philippe Coval <philippe.coval@osg.samsung.com>
2016-01-23 21:45:24 +0100

    WIP: add: rules/include/11-sunxi.mk (sandbox/pcoval/wip)
    Change-Id: I2612ac9b2a183e5b28e6cdb307b363329c3ebb70
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ rules/include/11-sunxi.mk
@@ -0,0 +1,28 @@
+#! /usr/bin/make -f
+# Author: Philippe Coval <philippe.coval@osg.samsung.com>
+# ex: set tabstop=4 noexpandtab:
+
+SHELL=/bin/bash
+V=1
+
+root_bsp=sunxi
+bsp?=${root_bsp}
+MACHINE?=olinuxino-a20
+machine?=${MACHINE}
+machines?=${machine}
+os?=tizen
+os_profile?=common
+distro?=${os}-distro
+extra?=
+init_name?=${os}-${os_profile}
+base_image?=core-image-minimal
+image?=oic-demo-image
+images?=${base_image} \
+ ${image} \
+ ${os}-${os_profile}-${base_image} \
+ tizen-common-core-image-crosswalk \
+ tizen-common-core-image-crosswalk-dev
+
+sources_layers_conf+=$(sort $(wildcard sources/meta-*/conf/layer.conf))
+#sources_layers_conf+=meta-tizen-${bsp}/conf/layer.conf
+
