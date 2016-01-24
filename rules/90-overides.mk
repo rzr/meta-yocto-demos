75a4b06995ef6e1f9cf9c5ceae52159ee5d423a3
Philippe Coval <philippe.coval@osg.samsung.com>
2016-01-24 22:40:49 +0100

    WIP: mod: rules/90-overides.mk (sandbox/pcoval/wip)
    Change-Id: I2c0304ad5efeb30a31a2b21f28ae2095e9212f05
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/90-overides.mk
+++ rules/90-overides.mk
@@ -21,11 +21,11 @@ rule/overide/patch/tizen-distro/todo: sources/tizen-distro/
 	 sources/tizen-distro/meta-tizen/meta-tizen-micro/meta-tizen-micro-raspberrypi/recipes-image/tizen-micro-rpi-hwup-image.bb 
 	-rm -rfv sources/tizen-distro/meta-tizen/meta-tizen-common-base/recipes-image/images/
 
-rule/overide/patch/meta-oic:  sources/meta-oic/
+rule/overide/patch/meta-oic/master:  sources/meta-oic/
 	-mv -v $</recipes-kernel/linux/linux-yocto_3.19.bbappend \
 	$</recipes-kernel/linux/linux-yocto_3.17.bbappend
 
-rule/overide/patch: rule/sources rule/overide/patch/tizen-distro rule/overide/patch/meta-oic rule/overide/patch/meta-${bsp}/master
+rule/overide/patch: rule/sources rule/overide/patch/tizen-distro rule/overide/patch/meta-oic/master rule/overide/patch/meta-${bsp}/master
 
 rule/overide/sources: rule/sources rule/done/patch
 
@@ -34,18 +34,18 @@ rule/overide/configure-conf: rule/configure-conf rule/overide/sources
 #	ls -l sources/meta-*${bsp}/conf/machine/${MACHINE}.conf
 
 rule/overide/packages: \
- rule/bitbake/task/weston \
- rule/bitbake/task/cogl-1.0 \
- rule/bitbake/task/cairo \
- rule/bitbake/task/harfbuzz \
- rule/bitbake/task/wayland \
- rule/bitbake/task/clutter-1.0 \
- rule/bitbake/task/efl \
- rule/bitbake/task/gstreamer \
- rule/bitbake/task/iotivity \
- rule/bitbake/task/iotivity-simple-client \
- rule/bitbake/task/iotivity-example \
- rule/bitbake/task/iotivity-sensorboard
+ rule/bitbake/build/weston \
+ rule/bitbake/build/cogl-1.0 \
+ rule/bitbake/build/cairo \
+ rule/bitbake/build/harfbuzz \
+ rule/bitbake/build/wayland \
+ rule/bitbake/build/clutter-1.0 \
+ rule/bitbake/build/efl \
+ rule/bitbake/build/gstreamer \
+ rule/bitbake/build/iotivity \
+ rule/bitbake/build/iotivity-simple-client \
+ rule/bitbake/build/iotivity-example \
+ rule/bitbake/build/iotivity-sensorboard
 	@echo "success: $@" 
 
 rule/local/clean: rule/bitbake/cleanall/tizen-platform-wrapper \
