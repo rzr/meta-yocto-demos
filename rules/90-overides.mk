6ee6c0d35350661f1a541188767548ed69665b60
Philippe Coval <philippe.coval@osg.samsung.com>
2016-01-24 01:11:08 +0100

    WIP: mod: rules/90-overides.mk (sandbox/pcoval/wip)
    Change-Id: I137974e6215fea26ca9111768b4cbe737c5987d7
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/90-overides.mk
+++ rules/90-overides.mk
@@ -25,13 +25,6 @@ rule/overide/patch/meta-oic:  sources/meta-oic/
 	-mv -v $</recipes-kernel/linux/linux-yocto_3.19.bbappend \
 	$</recipes-kernel/linux/linux-yocto_3.17.bbappend
 
-local/todo:
-	cd  sources/meta-raspberrypi/ && \
-	git cherry-pick 54c5451a04a2b6601ca729038780d4e4eb69437e
-
-	cd  sources/meta-raspberrypi/ && \
-	git cherry-pick	1949a0d5ba134036a590a41fd414f3bdd7ecee9e
-
 rule/overide/patch/%:
 	$(info no patch for $@)
 
@@ -58,13 +51,6 @@ rule/overide/configure-conf: rule/configure-conf rule/overide/sources
 	ls -l ${conf_file}
 #	ls -l sources/meta-*${bsp}/conf/machine/${MACHINE}.conf
 
-
-rule/wip/patch: rule/overide/patch/meta-raspberrypi
-	mv sources/tizen-distro/meta-tizen sources/tizen-distro/meta-tizen.orig
-	ln -fs \
- /home/philippe/var/cache/url/git/ssh/review.tizen.org/scm/bb/meta-tizen/src/meta-tizen \
- sources/tizen-distro/meta-tizen
-
 rule/overide/packages: \
  rule/bitbake/task/weston \
  rule/bitbake/task/cogl-1.0 \
@@ -82,41 +68,6 @@ rule/overide/packages: \
 
 rule/local/clean: rule/bitbake/cleanall/tizen-platform-wrapper \
  rule/bitbake/cleanall/tizen-platform-config
-
-
-rule/wip/cleanall: \
- rule/bitbake/cleanall/bluetooth-tools \
- rule/bitbake/cleanall/config-image \
- rule/bitbake/cleanall/desktop-skin \
- rule/bitbake/cleanall/gumd \
- rule/bitbake/cleanall/initramfs-live-boot \
- rule/bitbake/cleanall/initramfs-live-install \
- rule/bitbake/cleanall/initramfs-live-install-efi \
- rule/bitbake/cleanall/pam \
- rule/bitbake/cleanall/rpcbind \
- rule/bitbake/cleanall/rpm \
- rule/bitbake/cleanall/samba \
- rule/bitbake/cleanall/systemd-serialgetty \
- rule/bitbake/cleanall/tizen-platform-config \
- rule/bitbake/cleanall/tizen-platform-wrapper \
- rule/bitbake/cleanall/tlm \
- rule/bitbake/cleanall/udev-extraconf \
- rule/bitbake/cleanall/weston \
- rule/bitbake/cleanall/weston-init \
- rule/bitbake/cleanall/wpa-supplicant \
- #eol
-
-rule/wip/rebuild: rule/wip/cleanall \
- rule/bitbake/tlm
-
-rule/wip: \
- rule/bitbake/cleanall/userland rule/bitbake/task/userland \
- rule/bitbake/cleanall/cairo rule/bitbake/task/cairo \
- rule/bitbake/cleanall/harfbuzz rule/bitbake/task/harfbuzz \
- rule/bitbake/cleanall/config-image rule/bitbake/task/config-image
-
-rule/wip/help:
-	make rule/bitbake/args ARGS="-e" | grep BBPATH
 
 #rule/overide/image: rule/overide/packages rule/image
 
