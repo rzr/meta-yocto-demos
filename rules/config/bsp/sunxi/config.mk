ead58385f623c16242cc5fc378fa60e78f2f23eb
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-05 13:29:29 +0100

    WIP: add: rules/config/bsp/sunxi/config.mk (master)
    Change-Id: Ic59c76b2447e6b651de8361ba27ae5e359d03a94
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ rules/config/bsp/sunxi/config.mk
@@ -0,0 +1,33 @@
+#! /usr/bin/make -f
+# Author: Philippe Coval <philippe.coval@osg.samsung.com>
+# ex: set tabstop=4 noexpandtab:
+
+SHELL=/bin/bash
+V=1
+root_bsp=sunxi
+bsp?=${root_bsp}
+MACHINE?=olinuxino-a20
+machine?=${MACHINE}
+machines?=${machine}
+os?=tizen
+os_profile?=common
+distro?=${os}-distro
+extra?=
+init_name?=${os}-${os_profile}
+base_image?=core-image-minimal
+image?=oic-demo-image
+images?=${base_image} \
+ ${image} \
+ ${os}-${os_profile}-${base_image} \
+ tizen-common-core-image-crosswalk \
+ tizen-common-core-image-crosswalk-dev
+
+sources_layers_conf+=$(sort $(wildcard sources/meta-*/conf/layer.conf))
+#sources_layers_conf+=meta-tizen-${bsp}/conf/layer.conf
+
+rule/overide/patch/meta-sunxi/%: rule/done/patch-sunxi-mali
+	@echo "should happend once"
+
+rule/overide/patch-sunxi-mali: sources/meta-sunxi/conf/machine/include/sunxi-mali.inc
+       echo 'TUNE_FEATURES     = "arm armv7ve vfp  neon"' >> $<
+       echo 'TARGET_FPU        = "softfp"' >> $<
