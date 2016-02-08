618dfb9f1d1c6f0067c2433cd0cac7a03ed0d717
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-08 15:11:58 +0100

    WIP: mod: rules/90-overides.mk (master)
    Change-Id: I12be96779e760134bab4bf7f332d088f46662c2d
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/90-overides.mk
+++ rules/90-overides.mk
@@ -1,6 +1,3 @@
 #! /usr/bin/make -f
 # Author: Philippe Coval <philippe.coval@osg.samsung.com>
 # ex: set tabstop=4 noexpandtab:
-
-rule/overide/configure-conf: rule/configure-conf
-	ls -l ${conf_file}
