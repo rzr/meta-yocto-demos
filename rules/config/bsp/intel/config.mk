7fc35d045142ad62c2f6dc5940f7ccf672328495
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-08 17:09:33 +0100

    WIP: mod: rules/config/bsp/intel/config.mk (genivi)
    Change-Id: I5f659ce677eeb53c157502ae9609fbd373a2f7fe
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/config/bsp/intel/config.mk
+++ rules/config/bsp/intel/config.mk
@@ -20,15 +20,17 @@ image?=${base_image}
 images?=${base_image} \
  #eol
 
-sources_layers_conf+=$(sort $(wildcard sources/meta-*/conf/layer.conf))
-sources_layers_conf+=sources/meta-openembedded/meta-oe/conf/layer.conf
-sources_layers_conf+=sources/meta-openembedded/meta-ruby/conf/layer.conf
-sources_layers_conf+=sources/meta-ivi/meta-ivi/conf/layer.conf
-sources_layers_conf+=sources/meta-ivi/meta-ivi-bsp/conf/layer.conf
-#sources_layers_conf+=sources/meta-ivi/meta-ivi-demo/conf/layer.conf
+sources_name?=sources-${MACHINE}
+sources_layers_conf+=$(sort $(wildcard ${sources_name}/meta-*/conf/layer.conf))
+sources_layers_conf+=${sources_name}/meta-openembedded/meta-oe/conf/layer.conf
+sources_layers_conf+=${sources_name}/meta-openembedded/meta-ruby/conf/layer.conf
+sources_layers_conf+=${sources_name}/meta-ivi/meta-ivi/conf/layer.conf
+sources_layers_conf+=${sources_name}/meta-ivi/meta-ivi-bsp/conf/layer.conf
+#sources_layers_conf+=${sources_name}/meta-ivi/meta-ivi-demo/conf/layer.conf
 
 rule/overide/configure-conf: rule/configure-conf
 	sed -e 's|^DISTRO.*=.*|DISTRO ?= "poky-ivi-systemd"|g' -i ${conf_file}
+	cat rules/config/bsp/${bsp}/local.conf.in >> ${conf_file}
 
 rule/overide/patch/meta-genivi-demo: ${sources_dir}/meta-genivi-demo/conf/layer.conf 
 	sed -e 's|BBFILE_PRIORITY_genividemo = "7"|BBFILE_PRIORITY_genividemo = "8"|g' -i $<
