04cb290518a6205ef3240dcabe808e3a1eb78691
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-09 18:10:35 +0100

    build: refactored to build several machines in same branch
    Change-Id: Ia202daa102f69988b14d94e91afad68a2c0cc346
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ rules/60-aliases.mk
@@ -0,0 +1,21 @@
+#! /usr/bin/make -f
+# Author: Philippe Coval <philippe.coval@osg.samsung.com>
+# ex: set tabstop=4 noexpandtab:
+SELF?=${CURDIR}/rules/60-tasks.mk
+
+
+configure: rule/overide/configure
+
+rebuild: rule/overide/rebuild
+	date
+
+all: rule/overide/all
+	date
+
+clean: rule/overide/clean
+
+cleanall: rule/overide/cleanall
+
+help: rule/overide/help
+
+setup: rule/overide/setup
