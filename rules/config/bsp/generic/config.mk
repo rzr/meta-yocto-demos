edbd9e6514546956728e358e16f8ac2c1c241f0e
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-18 07:13:33 +0100

    WIP: mod: rules/config/bsp/generic/config.mk (sandbox/pcoval/wip)
    Change-Id: I43ccebbeb306e3b215585e207ddd8179873bc177
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/config/bsp/generic/config.mk
+++ rules/config/bsp/generic/config.mk
@@ -4,21 +4,23 @@
 
 SHELL=/bin/bash
 V=1
-
-bsp?=generic
-board_family?=${bsp}x86
-board_variant?=64
-MACHINE?=${board_family}-${board_variant}
+root_bsp=generic
+bsp?=${root_bsp}
+MACHINE?=${bsp}x86-64
 machine?=${MACHINE}
 machines?=${machine}
-os?=oe
-os_profile?=
-distro?=poky
+os?=tizen
+os_profile?=common
+distro?=${os}-distro
 extra?=
-init_name?=${os}${os_profile}
+init_name?=${os}-${os_profile}
 base_image?=core-image-minimal
-image?=${base_image}
+image?=oic-demo-image
 images?=${base_image} \
- #eol
-sources_layers_conf+=$(sort $(wildcard sources/meta-*/conf/layer.conf))
+ ${image} \
+ ${os}-${os_profile}-${base_image} \
+ tizen-common-core-image-crosswalk \
+ tizen-common-core-image-crosswalk-dev
 
+sources_layers_conf+=$(sort $(wildcard sources/meta-*/conf/layer.conf))
+#sources_layers_conf+=meta-tizen-${bsp}/conf/layer.conf
