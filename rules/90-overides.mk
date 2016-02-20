28248f7af5964252f77579d06e47aabdd5c51c52
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-20 20:31:09 +0100

    build: commented overide example to use bsp one if any
    Change-Id: I533a67b94920c17cfefac09964bdcf05875f65e3
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/90-overides.mk
+++ rules/90-overides.mk
@@ -2,5 +2,5 @@
 # Author: Philippe Coval <philippe.coval@osg.samsung.com>
 # ex: set tabstop=4 noexpandtab:
 
-rule/overide/configure-conf: rule/configure-conf
-	ls -l ${conf_file}
+#rule/overide/configure-conf: rule/configure-conf
+#	ls -l ${conf_file}
