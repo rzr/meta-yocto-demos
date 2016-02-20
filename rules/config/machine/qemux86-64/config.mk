1acb367957e67e119fb0d9b67126abd105d9ba44
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-20 20:31:09 +0100

    config: add qemux86-64 machine
    Change-Id: Iabb936c1130a69fc39564ab93e46742678ad2573
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ rules/config/machine/qemux86-64/config.mk
@@ -0,0 +1,10 @@
+#! /usr/bin/make -f
+# Author: Philippe Coval <philippe.coval@osg.samsung.com>
+# ex: set tabstop=4 noexpandtab:
+
+bsp?=generic
+board_family?=qemux86
+board_variant?=64
+MACHINE?=${board_family}-${board_variant}
+
+include rules/config/bsp/${bsp}/config.mk
