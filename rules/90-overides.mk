a9f9af2209bd2b65dc78bb4ce4c3955533b26c5d
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-21 01:02:02 +0100

    build: commented examples
    Change-Id: Iba64f9ff6b702ade91417f851f29a7d1654279ac
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/90-overides.mk
+++ rules/90-overides.mk
@@ -2,5 +2,8 @@
 # Author: Philippe Coval <philippe.coval@osg.samsung.com>
 # ex: set tabstop=4 noexpandtab:
 
+#rule/overide/all:
+#	$(warning # log: skip building ${@F}: ${ARG})
+
 #rule/overide/configure-conf: rule/configure-conf
 #	ls -l ${conf_file}
