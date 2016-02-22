39ac8e6274052f75aa4cd5d2e1852703cc286b4a
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-22 10:14:57 +0100

    build: overide, include config again (BSP)
    Change-Id: I5667c41e6ba71145fb09b3fc8a109225db470606
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/90-overides.mk
+++ rules/90-overides.mk
@@ -2,8 +2,18 @@
 # Author: Philippe Coval <philippe.coval@osg.samsung.com>
 # ex: set tabstop=4 noexpandtab:
 
+-include rules/config/bsp/${BSP}/config.mk
+
 #rule/overide/all:
 #	$(warning # log: skip building ${@F}: ${ARG})
 
+#rule/overide/image:
+#	$(warning # log: skip building ${@F}: ${ARG})
+
+#rule/overide/list-images:
+#	$(info # log: skip $@)
+
 #rule/overide/configure-conf: rule/configure-conf
 #	ls -l ${conf_file}
+
+#rule/overide/pre: rule/overide/bsps rule/pre
