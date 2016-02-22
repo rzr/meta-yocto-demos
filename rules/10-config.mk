8151d5942fef12659226fdf83afe7f5fc3dd9274
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-22 10:14:57 +0100

    config: set minimal values as example (default will be used)
    Change-Id: I01dfe439c5915fff5f7af3f1548ce0a9ccb634e4
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/10-config.mk
+++ rules/10-config.mk
@@ -2,27 +2,9 @@
 # Author: Philippe Coval <philippe.coval@osg.samsung.com>
 # ex: set tabstop=4 noexpandtab:
 
+
 SHELL=/bin/bash
 V=1
-root_bsp=generic
-bsp?=${root_bsp}
-board_family?=qemu
-board_variant?=x86
-MACHINE?=${board_family}${board_variant}
-machine?=${MACHINE}
-machines?=${machine} ${machine}-64
 
-os?=oe
-os_profile?=
+MACHINE?=qemux86
 distro?=poky
-extra?=
-init_name?=${os}
-base_image?=core-image-minimal
-image?=${base_image}
-images?=${image} ${image}-dev
-
-sources_name?=sources-${MACHINE}
-sources_layers_conf?=$(sort $(wildcard ${sources_name}/meta-*/conf/layer.conf))
-
-sources_layers_conf+=
-branch?=master
