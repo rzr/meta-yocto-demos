5bde3134991754347ec7d54ae869e38c5c60069f
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-22 15:15:16 +0100

    config: set intel-corei7-64 as default
    Change-Id: Id8bcd398d2cf1236aa86b71175bdeebbe2d59c5e
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/10-config.mk
+++ rules/10-config.mk
@@ -1,12 +1,4 @@
-#! /usr/bin/make -f
-# Author: Philippe Coval <philippe.coval@osg.samsung.com>
-# ex: set tabstop=4 noexpandtab:
-
-
-SHELL=/bin/bash
-V=1
-
-MACHINE?=qemux86
+MACHINE?=intel-corei7-64
 distro?=poky
 
 image?=demo-image
