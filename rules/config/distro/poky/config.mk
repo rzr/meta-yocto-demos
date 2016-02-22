08fa472f1e2689d789764cb5a43ad403fa060ac3
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-22 10:25:44 +0100

    poky: isolate distro specific in separate file
    Change-Id: Ib636dae801fd05f66ef1ff000da7be79680463e8
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ rules/config/distro/poky/config.mk
@@ -0,0 +1,29 @@
+#! /usr/bin/make -f
+# Author: Philippe Coval <philippe.coval@osg.samsung.com>
+# ex: set tabstop=4 noexpandtab:
+
+SHELL=/bin/bash
+V=1
+bsp_family?=generic
+bsp_variant?=
+bsp?=${bsp_family}${bsp_variant}
+board_family?=qemu
+board_variant?=x86
+MACHINE?=${board_family}${board_variant}
+machine?=${MACHINE}
+machines?=${machine} ${machine}-64
+
+os?=oe
+os_profile?=
+distro?=poky
+extra?=
+init_name?=${os}
+base_image?=core-image-minimal
+image?=${base_image}
+images?=${image} ${image}-dev
+
+sources_name?=sources-${MACHINE}
+sources_layers_conf?=$(sort $(wildcard ${sources_name}/meta-*/conf/layer.conf))
+
+sources_layers_conf+=
+branch?=master
