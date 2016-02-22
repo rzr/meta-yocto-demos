c14bc30afc5c2945674bf91e7cba8dac8ff9ad1c
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-22 10:14:58 +0100

    config: just define qemux86 machine
    Change-Id: Id677ae6ab31b01f286066b0a7ad6b4fe1039cc51
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/config/machine/qemux86/config.mk
+++ rules/config/machine/qemux86/config.mk
@@ -1,10 +1,2 @@
-#! /usr/bin/make -f
-# Author: Philippe Coval <philippe.coval@osg.samsung.com>
-# ex: set tabstop=4 noexpandtab:
-
 bsp?=generic
-board_family?=qemu
-board_variant?=x86
-MACHINE?=${board_family}${board_variant}
-
-include rules/config/bsp/${bsp}/config.mk
+MACHINE?=qemux86
