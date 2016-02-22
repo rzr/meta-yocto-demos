0d024c4fc789bebb252c34f267fd3f796c16be51
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-22 16:04:34 +0100

    poky: config: generic: use generated format
    Change-Id: Id7120adb5db7188c860e19c3dea19d455d27676b
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/config/machine/qemux86-64/config.mk
+++ rules/config/machine/qemux86-64/config.mk
@@ -1,8 +1,2 @@
-#! /usr/bin/make -f
-# Author: Philippe Coval <philippe.coval@osg.samsung.com>
-# ex: set tabstop=4 noexpandtab:
-
 bsp?=generic
-board_family?=qemux86
-board_variant?=64
-MACHINE?=${board_family}-${board_variant}
+MACHINE?=qemux86-64
