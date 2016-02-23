bc8c40dc548710b4dde911608903136879c2feb8
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-23 11:27:08 +0100

    agl: set intel-corei7-64 as default target (MinnowMax)
    Change-Id: Ife2e7001cf8dd7e34c834f890e09de5d3a55845a
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/10-config.mk
+++ rules/10-config.mk
@@ -1,10 +1,2 @@
-#! /usr/bin/make -f
-# Author: Philippe Coval <philippe.coval@osg.samsung.com>
-# ex: set tabstop=4 noexpandtab:
-
-
-SHELL=/bin/bash
-V=1
-
-MACHINE?=qemux86
+MACHINE?=intel-corei7-64
 distro?=poky
