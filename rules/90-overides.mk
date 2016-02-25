cfa0ebcd8f42d00fa9f5d07e7b6210398de8b3c7
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-25 23:56:17 +0100

    WIP: mod: rules/90-overides.mk (sandbox/pcoval/genivi)
    Change-Id: I0f8633fc7a4863a87028a0e3a9707fa8cd4e3d15
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/90-overides.mk
+++ rules/90-overides.mk
@@ -1,19 +1,3 @@
 #! /usr/bin/make -f
 # Author: Philippe Coval <philippe.coval@osg.samsung.com>
 # ex: set tabstop=4 noexpandtab:
-
--include rules/config/bsp/${bsp}/config.mk
-
-#rule/overide/all:
-#	$(warning # log: skip building ${@F}: ${ARG})
-
-#rule/overide/image:
-#	$(warning # log: skip building ${@F}: ${ARG})
-
-#rule/overide/list-images:
-#	$(info # log: skip $@)
-
-#rule/overide/configure-conf: rule/configure-conf
-#	ls -l ${conf_file}
-
-#rule/overide/pre: rule/overide/bsps rule/pre
