46cfe5738da2fd7dc41fd849d79f19a88bc0cdc0
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-26 10:05:47 +0100

    tizen: add default distro config file
    Change-Id: If8a010a559d27d7c1daa345caf5b5a197570d00b
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ rules/config/distro/tizen-distro/config.mk
@@ -0,0 +1,40 @@
+#! /usr/bin/make -f
+# Author: Philippe Coval <philippe.coval@osg.samsung.com>
+# ex: set tabstop=4 noexpandtab:
+
+SHELL=/bin/bash
+V=1
+
+bsp_family?=generic
+bsp_variant?=
+bsp?=${bsp_family}${bsp_variant}
+board_vendor?=${bsp}
+board_family?=${board_vendor}x86
+board_variant?=
+board_alias?=${board_family}${board_variant}
+MACHINE?=${board_family}${board_variant}
+machine?=${MACHINE}
+machines?=${machine} ${machine}-64
+
+os?=tizen
+os_profile?=common
+distro?=${os}-distro
+distro_branch?=${os}
+init_name?=${os}-${os_profile}
+init_build_env?=${sources_dir}/${distro}/${init_name}-init-build-env
+
+extra?=
+init_name?=${os}
+image_type?=core
+image?=${init_name}-${image_type}-image-minimal
+base_image?=${init_name}-${image_type}-image-minimal
+image?=${base_image}
+images?=${image} ${image}-dev
+
+sources_name?=sources-${MACHINE}
+sources_layers_conf?=$(sort $(wildcard ${sources_name}/meta-*/conf/layer.conf))
+
+sources_layers_conf+=
+branch?=master
+
+bsp_machines_dir?=${distro_machines_dir}
