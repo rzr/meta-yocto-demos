deeaf9654cc24eeea181c35fe1a525bc4847b799
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-29 12:07:03 +0100

    raspberrypi: remove unsupported machines by dizzy layer
    Change-Id: Id886317ad87a0a1a097a67416dbce861c2d80ab3
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/config/bsp/raspberrypi/config.mk
+++ rules/config/bsp/raspberrypi/config.mk
@@ -2,9 +2,6 @@
 # Author: Philippe Coval <philippe.coval@osg.samsung.com>
 # ex: set tabstop=4 noexpandtab:
 
-SHELL=/bin/bash
-V=1
-
 bsp_family?=raspberrypi
 bsp_variant?=
 bsp?=${bsp_family}${bsp_variant}
@@ -17,6 +14,8 @@ board_alias?=${board_family}${board_variant}
 MACHINE?=${board_family}${board_variant}
 
 machine?=${MACHINE}
-machines?=${machine} ${bsp} ${bsp}2 ${bsp}0
+machines?=${machine} ${bsp}
 
 base_image?=rpi-hwup-image
+sources_name?=sources-${MACHINE}
+
