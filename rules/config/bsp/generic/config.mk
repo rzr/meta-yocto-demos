d6bee3c7b8ddca16444526dc34e953747186ba40
Philippe Coval <philippe.coval@osg.samsung.com>
2016-03-30 15:21:07 +0200

    WIP: mod: rules/config/bsp/generic/config.mk (sandbox/pcoval/genivi)
    Change-Id: Ia325e2481a498ed1830dbcfc39a9e4d19b3180e6
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/config/bsp/generic/config.mk
+++ rules/config/bsp/generic/config.mk
@@ -5,44 +5,15 @@
 SHELL=/bin/bash
 V=1
 
-
 bsp_family?=generic
 bsp_variant?=
 bsp?=${bsp_family}${bsp_variant}
 
 board_vendor?=
 board_family?=${bsp}x86
-board_variant?=-64
+board_variant?=
 board_alias?=${board_family}${board_variant}
 
 MACHINE?=${board_family}${board_variant}
 
 bsp_machines_dir?=${distro_machines_dir}
-
-os?=oe
-os_profile?=
-distro?=poky
-distro_conf?=${distro}-ivi-systemd
-extra?=
-init_name?=${os}${os_profile}
-base_image?=genivi-demo-platform
-image?=${base_image}
-images?=${base_image} \
- #eol
-
-sources_name?=sources-${MACHINE}
-sources_layers_conf+=$(sort $(wildcard ${sources_name}/meta-*/conf/layer.conf))
-sources_layers_conf+=${sources_name}/meta-openembedded/meta-oe/conf/layer.conf
-sources_layers_conf+=${sources_name}/meta-openembedded/meta-ruby/conf/layer.conf
-sources_layers_conf+=${sources_name}/meta-ivi/meta-ivi/conf/layer.conf
-sources_layers_conf+=${sources_name}/meta-ivi/meta-ivi-bsp/conf/layer.conf
-#sources_layers_conf+=${sources_name}/meta-ivi/meta-ivi-demo/conf/layer.conf
-
-rule/overide/configure-conf: rule/configure-conf
-	sed -e 's|^DISTRO.*=.*|DISTRO ?= "poky-ivi-systemd"|g' -i ${conf_file}
-
-rule/overide/patch/meta-genivi-demo: ${sources_name}/meta-genivi-demo/conf/layer.conf 
-	sed -e 's|BBFILE_PRIORITY_genividemo = "7"|BBFILE_PRIORITY_genividemo = "8"|g' -i $<
-
-rule/overide/patch: rule/overide/patch/meta-genivi-demo
-
