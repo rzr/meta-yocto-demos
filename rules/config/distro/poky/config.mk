83b1251c926479a8f4bacb17e70942dc5a53f17d
Philippe Coval <philippe.coval@osg.samsung.com>
2016-03-30 15:21:10 +0200

    WIP: mod: rules/config/distro/poky/config.mk (sandbox/pcoval/genivi)
    Change-Id: I2d4e0aaea2c604ab96044757357319e3e30d08bf
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/config/distro/poky/config.mk
+++ rules/config/distro/poky/config.mk
@@ -13,21 +13,38 @@ MACHINE?=${board_family}${board_variant}
 machine?=${MACHINE}
 machines?=${machine} ${machine}-64
 
+
 os?=oe
 os_profile?=
-distro?=poky
+distro=poky
+distro_conf?=${distro}-ivi-systemd
 extra?=
-init_name?=${os}
-base_image?=core-image-minimal
+init_name?=${os}${os_profile}
+base_image?=genivi-demo-platform
 image?=${base_image}
-images?=${image} \
- ${base_image} \
- core-image-minimal \
- core-image-minimal-dev \
- core-image-weston
-
-sources_name?=sources-${MACHINE}
-sources_layers_conf?=$(sort $(wildcard ${sources_name}/meta-*/conf/layer.conf))
+images?=${base_image} \
+ #eol
 
 sources_layers_conf+=
 branch?=master
+
+
+sources_name?=sources-${MACHINE}
+sources_layers_conf+=$(sort $(wildcard ${sources_name}/meta-*/conf/layer.conf))
+sources_layers_conf+=${sources_name}/meta-openembedded/meta-oe/conf/layer.conf
+sources_layers_conf+=${sources_name}/meta-openembedded/meta-ruby/conf/layer.conf
+sources_layers_conf+=${sources_name}/meta-openembedded/meta-filesystems/conf/layer.conf
+sources_layers_conf+=${sources_name}/meta-ivi/meta-ivi/conf/layer.conf
+sources_layers_conf+=${sources_name}/meta-ivi/meta-ivi-bsp/conf/layer.conf
+
+#sources_layers_conf+=${sources_name}/meta-ivi/meta-ivi-demo/conf/layer.conf
+
+rule/overide/configure-conf: rule/configure-conf
+	sed -e 's|^DISTRO.*=.*|DISTRO ?= "poky-ivi-systemd"|g' -i ${conf_file}
+	cat rules/config/distro/${distro}/local.conf.in >> ${conf_file}
+
+rule/overide/patch/meta-genivi-demo: ${sources_name}/meta-genivi-demo/conf/layer.conf
+	sed -e 's|BBFILE_PRIORITY_genividemo = "7"|BBFILE_PRIORITY_genividemo = "8"|g' -i $<
+
+rule/overide/patch: rule/overide/patch/meta-genivi-demo
+
