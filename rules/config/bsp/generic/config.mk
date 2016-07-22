a493d103a1ef9b034c5009f5841ee8f73e8f7a6d
Philippe Coval <philippe.coval@osg.samsung.com>
2016-07-23 00:09:01 +0200

    poky: set qemux86 as default machine of generic bsp
    Change-Id: Ia527ef84320403f2649477c6fc5f798eb5e19c52
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/config/bsp/generic/config.mk
+++ rules/config/bsp/generic/config.mk
@@ -9,11 +9,13 @@ bsp_family?=generic
 bsp_variant?=
 bsp?=${bsp_family}${bsp_variant}
 
-board_vendor?=
-board_family?=${bsp}x86
+board_vendor?=qemu
+board_family?=x86
 board_variant?=
-board_alias?=${board_family}${board_variant}
+board_alias?=${board_vendor}${board_variant}
 
-MACHINE?=${board_family}${board_variant}
+MACHINE?=${board_vendor}${board_family}${board_variant}
 
+# only valid for distro shiping bsp
 bsp_machines_dir?=${distro_machines_dir}
+
