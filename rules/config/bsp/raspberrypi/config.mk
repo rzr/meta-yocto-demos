ff69a74e94f3bf62fc3efcaff31b371395c53a46
Philippe Coval <philippe.coval@osg.samsung.com>
2016-11-20 10:48:07 +0100

    raspberrypi: Hack to use to deezy
    Change-Id: Iac79ce9467422af048a6ca28d467f63b3dd9adc8
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/config/bsp/raspberrypi/config.mk
+++ rules/config/bsp/raspberrypi/config.mk
@@ -20,3 +20,15 @@ machine?=${MACHINE}
 machines?=${machine} ${bsp}
 
 base_image?=rpi-hwup-image
+sources_name?=sources-${MACHINE}
+sources_layers_conf+=$(sort $(wildcard ${sources_name}/meta-*/conf/layer.conf))
+sources_dir?=${CURDIR}/${sources_name}
+
+rule/override/patch/meta-raspberrypi/master: ${sources_dir} ${sources_dir}/meta-raspberrypi
+	-sed -e 's|STAGING_KERNEL_BUILDDIR|STAGING_KERNEL_DIR|g' -i  \
+	 ${<}/meta-raspberrypi/classes/linux-raspberrypi-base.bbclass
+	-sed -e 's|get_kernelversion_file|get_kernelversion|g' -i  \
+	${<}/meta-raspberrypi/classes/linux-raspberrypi-base.bbclass
+	ls $<
+
+override_tasks+=rule/override/patch/meta-raspberrypi/master
