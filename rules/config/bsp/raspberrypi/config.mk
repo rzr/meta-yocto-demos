ad2ff2af0ad3930ce1c5aeab99a04bf714b73b04
Philippe Coval <philippe.coval@osg.samsung.com>
2017-05-28 13:10:25 +0200

    WIP: mod: rules/config/bsp/raspberrypi/config.mk (sandbox/pcoval/raspberrypi0)
    Change-Id: Ifc4f3c4371d5d4246f965151f56e21f526caa503
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/config/bsp/raspberrypi/config.mk
+++ rules/config/bsp/raspberrypi/config.mk
@@ -33,16 +33,16 @@ sources_layers_conf+=$(sort $(wildcard ${sources_name}/meta-*/conf/layer.conf))
 sources_layers_conf+=meta-tizen-raspberrypi/conf/layer.conf
 
 
-rule/overide/patch/meta-raspberrypi/master: ${sources_name}/meta-raspberrypi
+rule/override/patch/meta-raspberrypi/master: ${sources_name}/meta-raspberrypi
 	-sed -e 's|STAGING_KERNEL_BUILDDIR|STAGING_KERNEL_DIR|g' -i  \
 	 ${sources_name}/meta-raspberrypi/classes/linux-raspberrypi-base.bbclass
 	-sed -e 's|get_kernelversion_file|get_kernelversion|g' -i  \
 	${sources_name}/meta-raspberrypi/classes/linux-raspberrypi-base.bbclass
 
-rule/overide/patch/meta-raspberrypi/dizzy: ${sources_name}/meta-raspberrypi
+rule/override/patch/meta-raspberrypi/dizzy: ${sources_name}/meta-raspberrypi
 	mkdir -p recipes-graphics/cairo
 	echo 'CFLAGS_append_raspberrypi="-I\${STAGING_INCDIR}/interface/vcos/pthreads/ -I\${STAGING_INCDIR}/interface/vmcs_host/linux/"' \
 	> recipes-graphics/cairo/cairo_1.12.16.bbappend
 
-rule/overide/clean-bsp: rule/bitbake/cleanall/userland
+rule/override/clean-bsp: rule/bitbake/cleanall/userland
 	@echo "bsp=${bsp}"
