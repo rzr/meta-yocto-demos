fbc73721a15055f3024f804d0b5e3c59c2d2b0d5
Philippe Coval <philippe.coval@osg.samsung.com>
2017-05-28 13:10:25 +0200

    WIP: renamed: from rules/90-overides.mk to rules/90-overrides.mk
    Change-Id: I96eed941cfbb93bf53a3a38a3b991ebe7728d61b
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/90-overrides.mk
+++ rules/90-overrides.mk
@@ -3,40 +3,40 @@
 # ex: set tabstop=4 noexpandtab:
 
 
-todo/rule/overide/patch/tizen-distro/ldconfig: ${sources_name}/tizen-distro/meta-tizen/
+todo/rule/override/patch/tizen-distro/ldconfig: ${sources_name}/tizen-distro/meta-tizen/
 	-grep -r '^[\ ]*ldconfig[\ ]*$$' $< | cut -d: -f1 | sort | uniq | while read t ; do \
  sed -e 's/^[ ]*ldconfig[ ]*$$/\[ \"\" != "\$$D\" \] || ldconfig # TODO/g' -i $$t  && echo "ldconfig: $$t" ; \
  done  | grep ldconfig
 
-todo/rule/overide/patch/tizen-distro: ${sources_name}/tizen-distro/
+todo/rule/override/patch/tizen-distro: ${sources_name}/tizen-distro/
 	-find $< -type f -exec sed -e 's/\[ \"x$$D\" == \"x\" \] && ldconfig/\[ \"\" != \"$$D\" \] || ldconfig/g' -i "{}" \;
 
 #	-cd $< && git diff
 
-rule/overide/patch/tizen-distro/todo: ${sources_name}/tizen-distro/
+rule/override/patch/tizen-distro/todo: ${sources_name}/tizen-distro/
 	install -d ${sources_name}/tizen-distro/meta-tizen/meta-tizen-micro/meta-tizen-micro-raspberrypi/recipes-image/
 	-mv -fv \
 	 ${sources_name}/tizen-distro/meta-tizen/meta-tizen-micro/recipes-image/raspberrypi2/rpi-hwup-image-tizen-micro.bb \
 	 ${sources_name}/tizen-distro/meta-tizen/meta-tizen-micro/meta-tizen-micro-raspberrypi/recipes-image/tizen-micro-rpi-hwup-image.bb 
 	-rm -rfv ${sources_name}/tizen-distro/meta-tizen/meta-tizen-common-base/recipes-image/images/
 
-rule/overide/patch/meta-oic/master:  ${sources_name}/meta-oic/
+rule/override/patch/meta-oic/master:  ${sources_name}/meta-oic/
 	-mv -v $</recipes-kernel/linux/linux-yocto_3.19.bbappend \
 	$</recipes-kernel/linux/linux-yocto_3.17.bbappend
 
-rule/overide/patch: rule/sources rule/overide/patch/tizen-distro rule/overide/patch/meta-oic/master rule/overide/patch/meta-${bsp}/master
+rule/override/patch: rule/sources rule/override/patch/tizen-distro rule/override/patch/meta-oic/master rule/override/patch/meta-${bsp}/master
 
-rule/overide/sources: rule/sources rule/done/patch
+rule/override/sources: rule/sources rule/done/patch
 
-rule/overide/configure-conf: rule/configure-conf rule/overide/configure-bsp rule/overide/sources
+rule/override/configure-conf: rule/configure-conf rule/override/configure-bsp rule/override/sources
 	ls -l ${conf_file}
 #	ls -l ${sources_name}/meta-*${bsp}/conf/machine/${MACHINE}.conf
 
 
-rule/overide/patch/tizen-distro: ${sources_name}
+rule/override/patch/tizen-distro: ${sources_name}
 	-find $< -type -f -exec sed -e 's/review.tizen.org/git.tizen.org/g' -i "{}" \;
 
-rule/overide/packages: \
+rule/override/packages: \
  rule/bitbake/build/cairo \
  rule/bitbake/build/clutter-1.0 \
  rule/bitbake/build/cogl-1.0 \
@@ -60,5 +60,5 @@ rule/local/clean: \
  rule/bitbake/cleanall/weston-init \
  #eol
 
-#rule/overide/image: rule/overide/packages rule/image
+#rule/override/image: rule/override/packages rule/image
 
