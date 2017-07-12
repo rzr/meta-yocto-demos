5b569bed3f0deaa9784e20040a064399e9df7597
Philippe Coval <philippe.coval@osg.samsung.com>
2017-07-12 14:24:38 +0200

    WIP: add: rules/config/bsp/fsl-arm/config.mk (meta/meta-fsl-arm/daisy)
    Change-Id: Iaa93a444a45bf8c99ad2061b8f1016eef43fea64
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ rules/config/bsp/fsl-arm/config.mk
@@ -0,0 +1,20 @@
+#! /usr/bin/make -f
+# Author: Philippe Coval <philippe.coval@osg.samsung.com>
+# ex: set tabstop=4 noexpandtab:
+
+SHELL=/bin/bash
+V=1
+
+bsp_family?=generic
+bsp_variant?=
+bsp?=${bsp_family}${bsp_variant}
+
+board_vendor?=qemu
+board_family?=${board_vendor}x86
+board_variant?=
+board_alias?=${board_vendor}${board_variant}
+
+MACHINE?=${board_family}${board_variant}
+
+# Only valid for distros not BSP's config
+bsp_machines_dir?=${distro_machines_dir}
