685c1c174e76747494803b5b1378e0e325e657c9
Philippe Coval <philippe.coval@osg.samsung.com>
2017-11-07 12:43:28 +0100

    WIP: mod: rules/30-default.mk (meta/meta-ocf-automotive/devel)
    Change-Id: I6261ac0b51d6142d2ea7da8ac13e4c82cfa7163e
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- rules/30-default.mk
+++ rules/30-default.mk
@@ -46,7 +46,7 @@ build_file?=${tmp_dir}/build.log
 sources_layers_conf?=$(sort $(wildcard ${sources_name}/meta-*/conf/layer.conf))
 sources_layers_conf+=./conf/layer.conf
 sources_layers_dirs?=$(dir ${sources_layers_conf})
-sources_layers?=$(sort $(shell dirname $(sources_layers_dirs)))
+sources_layers?=$(shell dirname $(sources_layers_dirs))
 
 init_name?=oe
 init_build_env?=${sources_dir}/${distro}/${init_name}-init-build-env
