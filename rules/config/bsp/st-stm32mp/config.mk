8cf816a1904258006740ee8803e14cbb17b28402
Philippe Coval <rzr@users.sf.net>
2019-12-17 22:10:33 +0100

    WIP: mod: rules/config/bsp/st-stm32mp/config.mk (meta/meta-st-stm32mp/master)
    Signed-off-by: Philippe Coval <rzr@users.sf.net>

--- rules/config/bsp/st-stm32mp/config.mk
+++ rules/config/bsp/st-stm32mp/config.mk
@@ -6,11 +6,11 @@ SHELL=/bin/bash
 V=1
 
 bsp_family?=stm32
-bsp_variant?=-mp
-bsp?=${bsp_family}${bsp_variant}
+bsp_variant?=mp
+bsp?=${bsp_family}-${bsp_variant}
 
 board_vendor?=${bsp_family}
-board_family?=mp
+board_family?=${board_vendor}${bsp_variant}
 board_variant?=1
 board_alias?=${board_vendor}${board_variant}
 
