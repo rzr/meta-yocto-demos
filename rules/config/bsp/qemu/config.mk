fbbfedf435f7778019e0b22d836b8d6ce7c26fd3
Philippe Coval <rzr@users.sf.net>
2019-12-22 19:05:51 +0100

    oe: Fetch openembedded-core
    Base: base/master
    Change-Id: Iba1380b6fb654f7f840932b0e5dc49fa6f0fcade
    Signed-off-by: Philippe Coval <rzr@users.sf.net>

--- rules/config/bsp/qemu/config.mk
+++ rules/config/bsp/qemu/config.mk
@@ -5,13 +5,13 @@
 SHELL=/bin/bash
 V=1
 
-bsp_family?=generic
+bsp_family?=qemu
 bsp_variant?=
 bsp?=${bsp_family}${bsp_variant}
 
-board_vendor?=qemu
+board_vendor?=${bsp_family}
 board_family?=${board_vendor}x86
-board_variant?=
+board_variant?=-64
 board_alias?=${board_vendor}${board_variant}
 
 MACHINE?=${board_family}${board_variant}
